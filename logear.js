cbfde3c6adfe25d5e09a78331a12552e179986f1
Ashleehernandez <149027004+Ashleehernandez@users.noreply.github.com>
2023-10-25 21:19:23 -0400

    INICIO DE SECCIONE
    FORMULARIO VALIDANDO USUARIO Y CONTRASEÑA CON EL LENGUAJE JAVASCRIPT

---
+++ logear.js
@@ -0,0 +1,17 @@
+
+ function logear()
+ {
+    
+    let user=document.getElementById("user").value;
+    let password=document.getElementById("password").value;
+  
+    if(user=="Ashlee" && password=="0830")
+    {
+        window.location= "bien.html";
+    }
+
+    else {
+        alert ("tu codigo no funciona jjjj");
+    }
+       
+ }  
